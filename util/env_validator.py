8ec5a931e1ecd3054fecad52dd254d966333e364
mayaracsimoes <mayaraciinho@gmail.com>
2025-06-20 20:55:51 -0300

    Revert "Api para popular base de dados do Mongo"
    This reverts commit 0b42fa77a72221879828cd50ed627d46d9cede82.

--- util/env_validator.py
+++
@@ -1,6 +0,0 @@
-import os
-def validar_env_vars():
-  required_vars = ['MONGO_URI', 'MONGO_DB', 'MONGO_COLLECTION']
-  for var in required_vars:
-      if not os.getenv(var):
-          raise EnvironmentError(f'Variável de ambiente "{var}" não está definida no .env')
